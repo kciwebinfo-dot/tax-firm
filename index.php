7cb6af91ea739c1ee66ba02255240385b2a8a197
kciwebinfo-dot <kciwebinfo@gmail.com>
2026-07-06 11:25:22 +0530

    Remove old root index file

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/session.php';
-require_once __DIR__ . '/includes/functions.php';
-
-if (!empty($_SESSION['user'])) {
-    redirect(app_url('modules/dashboard/index.php'));
-}
-
-redirect(app_url('modules/auth/login.php'));
