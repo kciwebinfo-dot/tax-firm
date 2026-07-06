35a03c4309d7445b13d8173d349f375643662414
kciwebinfo-dot <kciwebinfo@gmail.com>
2026-07-06 10:39:07 +0530

    Add phase 2 profile module

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,4 +1,9 @@
-<?php $user = current_user(); ?>
+<?php
+$user = current_user();
+$currentPath = str_replace('\\', '/', $_SERVER['SCRIPT_NAME'] ?? '');
+$isDashboard = str_contains($currentPath, '/modules/dashboard/');
+$isProfile = str_contains($currentPath, '/modules/profile/');
+?>
 <aside class="sidebar" id="sidebar">
     <div class="brand">
         <img src="<?= e($firm['logo'] ?? asset('img/logo.svg')) ?>" alt="Firm logo">
@@ -8,13 +13,13 @@
         </div>
     </div>
     <nav class="sidebar-nav">
-        <a class="active" href="<?= e(app_url('modules/dashboard/index.php')) ?>"><i class="fa-solid fa-chart-line"></i><span>Dashboard</span></a>
+        <a class="<?= $isDashboard ? 'active' : '' ?>" href="<?= e(app_url('modules/dashboard/index.php')) ?>"><i class="fa-solid fa-chart-line"></i><span>Dashboard</span></a>
         <a href="#"><i class="fa-solid fa-users"></i><span>Clients</span></a>
         <a href="#"><i class="fa-solid fa-file-invoice"></i><span>GST</span></a>
         <a href="#"><i class="fa-solid fa-receipt"></i><span>ITR</span></a>
         <a href="#"><i class="fa-solid fa-wallet"></i><span>Fees</span></a>
         <a href="#"><i class="fa-solid fa-user-shield"></i><span>Roles</span></a>
-        <a href="<?= e(app_url('modules/profile/index.php')) ?>"><i class="fa-solid fa-user-gear"></i><span>Profile</span></a>
+        <a class="<?= $isProfile ? 'active' : '' ?>" href="<?= e(app_url('modules/profile/index.php')) ?>"><i class="fa-solid fa-user-gear"></i><span>Profile</span></a>
         <a href="#"><i class="fa-solid fa-gear"></i><span>Settings</span></a>
     </nav>
 </aside>
