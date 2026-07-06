954104cd6024043fc5f2472a663301c95fbedfdd
kciwebinfo-dot <kciwebinfo@gmail.com>
2026-07-06 09:54:47 +0530

    Add layout templates

---
+++ layout/loader.php
@@ -0,0 +1,6 @@
+<div id="appLoader" class="app-loader">
+    <div class="loader-card">
+        <div class="spinner-border text-primary" role="status"></div>
+        <span>Loading workspace...</span>
+    </div>
+</div>
