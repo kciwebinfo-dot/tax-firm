df0070082b62287c244675bca2aeb071e2e3acf3
kciwebinfo-dot <kciwebinfo@gmail.com>
2026-07-06 09:52:08 +0530

    Add shared PHP helpers

---
+++ includes/flash.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Flash message helper.
+ */
+declare(strict_types=1);
+
+function flash_set(string $type, string $message): void
+{
+    $_SESSION['flash'] = ['type' => $type, 'message' => $message];
+}
+
+function flash_get(): ?array
+{
+    $flash = $_SESSION['flash'] ?? null;
+    unset($_SESSION['flash']);
+    return $flash;
+}
